410af439939ed4dba251f704429d753995c653c5
Bence Toth <tothbence9922@gmail.com>
2021-11-05 15:03:50 +0000

    Unnecessary configuration.getInstance call removed

--- cmd/kawe/main.go
+++ cmd/kawe/main.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/tothbence9922/kawe/internal/configuration"
 	simple "github.com/tothbence9922/kawe/internal/ping/simple/service"
 )
 
 func main() {
-	configuration.GetInstance()
-
-	samplePingConfig := configuration.PingConfiguration{Periodicity: 5, Target: "google.com:443"}
-	samplePingConfigs := make([]configuration.PingConfiguration, 1)
-	samplePingConfigs[0] = samplePingConfig
 
 	sampleService := new(simple.SimplePingerService)
 
 	sampleService.Configure()
-
-	fmt.Println(sampleService.String())
 
 	sampleService.StartMethods()
 }
